314a87e148937193e28ef9586408884b3de0ed44
YangHyeyoung <ansouon@gmail.com>
2021-09-13 09:28:17 +0900

    js string

--- day4/ex4.js
+++ day4/ex4.js
@@ -12,12 +12,13 @@ document.write('문자열 추출 2 : ' + text.slice(7, 11));
 document.write('<br>');
 result = text.replace('javascript','jsp');  //대소문자구분해서 일치하는 것을 변환.
 
+// 아래 내용은 메소드 연습 마치고 하겠습니다.
 let x = "John";     // string 타입 리터럴 'John'
 let y = new String("John");     // 객체 생성
 let z = new String("John");
-document.write(typeof x); document.write('<br>');
-document.write(typeof y); document.write('<br>');
-document.write(typeof z); document.write('<br>');
+document.write(typeof x); document.write('<br>');   // string
+document.write(typeof y); document.write('<br>');   // object
+document.write(typeof z); document.write('<br>');   // object
 document.write(x); document.write('<br>');
 document.write(y); document.write('<br>');
 document.write(z); document.write('<br>');
